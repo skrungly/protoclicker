6a2bd1bfb2a6b3b893d1e9ef06a4cbeb8eeeab99
skrungly <kingsleymcdonald@outlook.com>
2024-06-09 19:14:08 +0100

    add colour fade for held keys

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import board
 import keypad
 import math
+import time
 import usb_hid
 
 import neopixel
@@ -24,39 +25,74 @@ KEY_VALUES = (
 
 N_KEYS = len(KEY_PINS)
 
-COLOUR_OFF = 0x3f3f3f
-COLOUR_ON = 0xffffff
+COLOUR_OFF = (63, 63, 63)
+
+# as the keys are held, their colour should fade.
+COLOUR_PRESS = (63, 255, 0)
+COLOUR_HOLD = (0, 63, 255)
+
+FADE_STEPS = 16
+FADE_STEP_INTERVAL_NS = (2 * 10 ** 9) // FADE_STEPS  # total 2 seconds
+
+COLOUR_MATRIX = [
+    # generate a gradient between the key press colour and
+    # the final hold colour, using linear interpolation
+    # across the space of RGB colours.
+    tuple(int(COLOUR_PRESS[i] + step * (
+        COLOUR_HOLD[i] - COLOUR_PRESS[i]
+    ) / (FADE_STEPS - 1)) for i in range(3))  # r, g, b
+    for step in range(FADE_STEPS)
+]
 
 # list of integer gamma-correction values for accurate neopixel colours
 GAMMA_TABLE = [int(math.pow(i / 255, 2.8) * 255 + 0.5) for i in range(256)]
 
 
 def gamma_corrected(colour):
-    r = (colour & 0xff0000) >> 16
-    g = (colour & 0x00ff00) >> 8
-    b = colour & 0x0000ff
-
-    return (GAMMA_TABLE[r] << 16) + (GAMMA_TABLE[g] << 8) + GAMMA_TABLE[b]
+    r, g, b = colour
+    return GAMMA_TABLE[r], GAMMA_TABLE[g], GAMMA_TABLE[b]
 
 
-# end of constants. set up lights an keys!
-neopixels = neopixel.NeoPixel(NEOPIXEL_PIN, n=N_KEYS)
+# end of constants. set up lights and keys!
+neopixels = neopixel.NeoPixel(NEOPIXEL_PIN, n=N_KEYS, auto_write=False)
 neopixels.fill(COLOUR_OFF)
 
 keys = keypad.Keys(KEY_PINS, value_when_pressed=True, pull=True)
 keyboard = Keyboard(usb_hid.devices)
 
+keys_pressed = [None] * N_KEYS
+key_colours = [COLOUR_OFF] * N_KEYS
+
 # now begins the event loop
 while True:
-    event = keys.events.get()
+    time_now = time.monotonic_ns()
+    write_pixels = False
 
+    event = keys.events.get()
     if event:
         key = event.key_number
 
         if event.pressed:
             keyboard.press(KEY_VALUES[key])
-            neopixels[key] = gamma_corrected(COLOUR_ON)
+            keys_pressed[key] = time_now
+            # the colour for pressed keys is handled below
 
         if event.released:
             keyboard.release(KEY_VALUES[key])
-            neopixels[key] = gamma_corrected(COLOUR_OFF)
+            keys_pressed[key] = None
+            neopixels[key] = COLOUR_OFF
+            write_pixels = True
+
+    for key, held_since in enumerate(keys_pressed):
+        if not held_since:
+            continue
+
+        current_fade_step = (time_now - held_since) // FADE_STEP_INTERVAL_NS
+        current_colour = COLOUR_MATRIX[min(FADE_STEPS - 1, current_fade_step)]
+
+        if neopixels[key] != current_colour:
+            neopixels[key] = current_colour
+            write_pixels = True
+
+    if write_pixels:
+        neopixels.show()
